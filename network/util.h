30b24aa535df51957f0faeb2c8ba535ff71c7248
xzhangxian1008 <xzhangxian1008@foxmail.com>
2025-04-09 16:06:56 +0800

    save

--- network/util.h
+++ network/util.h
@@ -2,7 +2,16 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <cassert>
+#include <iostream>
 
 #pragma once
 
 constexpr uint16_t SERVER_PORT = 5762;
+
+inline void print__() { std::cout << std::endl; };
+
+template<typename T, typename... Types>
+void print__(const T& firstArg, const Types&... args) {
+    std::cout << firstArg << std::ends;
+    print__(args...);
+}
